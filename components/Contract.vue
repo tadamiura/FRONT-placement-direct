6ee48a31a9f8ae0a6501c4afbec936c9f5a5da91
Miura <tadamiura@gmail.com>
2021-01-24 14:53:31 +0100

    created component of contracts

---
+++ components/Contract.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>
+      <b-card :header="title" class="text-center">
+        <b-card-text>
+            le lien : {{ description }}
+        </b-card-text>
+      <hr />
+      </b-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "contract",
+    props: ["title", "description"]
+}
+</script>

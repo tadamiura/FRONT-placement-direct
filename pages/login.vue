52ec38543f17674e8608030deb8fb1406269887a
Miura <tadamiura@gmail.com>
2021-01-24 14:58:13 +0100

    created login page

---
+++ pages/login.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-4 is-offset-4">
+          <h2 class="title has-text-centered">Connexion</h2>
+
+          <b-notification v-if="error" type="is-warning">
+            {{ error }}
+          </b-notification>
+
+          <form method="post" @submit.prevent="login">
+            <b-field label="Email" label-for="email">
+              <b-input id="email" v-model="email"></b-input>
+            </b-field>
+            <b-field label="Password" label-for="password">
+              <b-input
+                id="password"
+                v-model="password"
+                type="password"
+                password-reveal
+              >
+              </b-input>
+            </b-field>
+            <div class="control">
+              <button type="submit" class="button is-dark is-fullwidth">
+                Se connecter
+              </button>
+            </div>
+          </form>
+
+          <div class="has-text-centered" style="margin-top: 20px">
+            <p>
+              Avez-vous un compte ?
+              <nuxt-link to="/register">S'inscrire</nuxt-link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: ''
+    }
+  },
+  methods: {
+    async login() {
+        try {
+            await this.$auth.loginWith('local', {
+                data: {
+                    email: this.email,
+                    password: this.password
+                }
+            })
+            this.$router.push('/')
+        } catch (e) {
+            this.error = e.response.data.message
+        }
+    }
+  }
+}
+</script>

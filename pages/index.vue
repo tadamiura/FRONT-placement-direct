f36f7086630781f90ffd39a66b0de97dff981975
Miura <tadamiura@gmail.com>
2021-01-24 14:55:14 +0100

    changed index.vue to display contracts

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,35 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        placement-direct
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <h1 class="display-1">Mes Contrats</h1>
+    <Contract v-for="contract in contracts" :title="contract.title" :description="contract.description" :key="contract.id"/>
   </div>
 </template>
 
 <script>
-export default {}
+import Contract from '@/components/Contract.vue'
+export default {
+  // middleware: 'auth',
+  components: {
+    Contract
+  },
+  data() {
+    return {
+      contracts: [],
+    }
+  },
+  mounted() {
+  },
+  async asyncData({ $axios, $auth }) {
+    let user = $auth.state.user
+    const contracts = await $axios.$get(
+      `http://localhost:8080/api/user/${user}/contract`
+    )
+    return { contracts };
+  }
+}
 </script>
-
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  li {
+    list-style: none;
+  }
 </style>
